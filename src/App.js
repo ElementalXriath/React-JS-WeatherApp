3658f956ca26e1cbea250f6c9ddfc1da8fdebeb5
Asleepless Knight <41958127+ElementalXriath@users.noreply.github.com>
2019-11-03 11:56:34 -0500

    Use Hooks in Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+
+const App = () => {
+
+const [count, SetCount] = useState(10);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={() => SetCount(currentCount => currentCount + 1)}></button>
+    <div>{count}</div>
     </div>
   );
 }
